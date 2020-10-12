84410faf82dc514ae202bfefdb98b3be7e20d943
ryoooarai <ryoooma.arai@gmail.com>
2020-10-12 23:28:20 +0900

    added view for result

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import BasicInfoForm from './components/BasicInfoForm'
 import QuestionaryForm from "./components/QuestionaryForm";
 import ConsulationDetailForm from "./components/ConsultationDetailForm";
+import ResultView from "./components/ResultView";
 
 export const routes = [
     {path: '', component: BasicInfoForm, name: 'basicInfoForm'},
     {path: '/questionary-form', component: QuestionaryForm, name: 'questionaryForm'},
-    {path: '/consultation-detail-form', component: ConsulationDetailForm, name: 'consultationDetailForm'}
+    {path: '/consultation-detail-form', component: ConsulationDetailForm, name: 'consultationDetailForm'},
+    {path: '/result-view', component: ResultView, name: 'resultView'},
 ]
